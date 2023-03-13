b4aa5875ad6bbe3cfa42899f6f3a421a55be6263
lhs0698 <yohs0698@naver.com>
2023-03-13 21:31:17 +0900

    영화 api 호출

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+
+  
+  
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9fb02562e080e36f57d6874f81a76246&targetDt=20230312`
+      )
+    ).json();
+    setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+  };
+
+  useEffect(() => {
+    getMovies();
+  },[]);
+
+  console.log(movies)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <div>영화 제목 : {movie.movieNm}</div>
+            <div>개봉일 : {movie.openDt}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
