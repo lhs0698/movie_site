5edab817b039d8a17ab6641521aa67a5b59f2554
lhs0698 <yohs0698@naver.com>
2023-03-17 21:49:13 +0900

    영화 누적관객수 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const HeaderForm = styled.nav`
+  width: 100%;
+  height: 70px;
+  background-color: #efefef;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Headerul = styled.ul`
+    list-style: none;
+`
+
 const Header = () => {
-    return(
-        <div>
-            <header>Header</header>
-        </div>
-    )
-}
+  return (
+    <HeaderForm>
+        <Headerul>
+          <li><Link to={"/info"} style={{ textDecoration: 'none' }}>일일 박스오피스</Link></li>
+          <li></li>
+        </Headerul>
+      
+    </HeaderForm>
+  );
+};
 
 export default Header;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from "react";
 
 const Movie = () => {
+  // 영화 api 일일 박스오피스
   const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
+    // 일별 박스오피스 api 날짜, yymmdd 형식
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
-    const day = date.getDate() - 1 ;
+    const day = date.getDate() - 1;
+    const toDate =
+      year +
+      ("00" + month.toString()).slice(-2) +
+      ("00" + day.toString()).slice(-2);
 
-    const toDate = year+(("00"+month.toString()).slice(-2))+(("00"+day.toString()).slice(-2));
-    // console.log(format)
     const json = await (
-      await fetch( 
-        `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9fb02562e080e36f57d6874f81a76246&targetDt=${toDate}` 
+      await fetch(
+        `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9fb02562e080e36f57d6874f81a76246&targetDt=${toDate}`
       )
     ).json();
     setMovies(json.boxOfficeResult.dailyBoxOfficeList);
@@ -22,17 +26,18 @@ const Movie = () => {
   useEffect(() => {
     getMovies();
   }, []);
-  
+
   return (
     <div>
-      <div>
-        {movies.map((movie, index) => (
-          <div key={index}>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <div>
             <div>영화 제목 : {movie.movieNm}</div>
-            <div>개봉일 : {movie.openDt}</div>
+            <div>개봉일 : ({movie.openDt})</div>
+            <div>누적 관객 수 : {movie.audiAcc}명</div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
